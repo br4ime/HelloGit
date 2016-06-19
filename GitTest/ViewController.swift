126bd18224a727ac1fa77d785af77826aaa3976d
Hans Huber <daniel.brehme@gmx.de>
2016-06-19 19:28:21 +0200

    initial commit from local box

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Hans Huber on 19.06.16.
+//  Copyright © 2016 Hans Huber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
